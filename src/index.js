8d458acf1bb5a8b31537199ce5f367bfbc503752
AlexanderWong <wongalex@umich.edu>
2018-11-25 17:10:25 -0800

    updated methods

--- src/index.js
+++ src/index.js
@@ -23,6 +23,14 @@ class App extends React.Component {
     );
   }
 
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+
+  componentDidUpdate() {
+    console.log("My component was just updated and rerendered.");
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat && !this.state.lng) {
       return <div>Error: {this.state.errorMessage}</div>;
